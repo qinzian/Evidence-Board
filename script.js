e1bb2305900c9be444d9363aeadf6188afd2cd4f
zion Niq <zion Niq>
2017-07-12 11:29:48 -0400

    added a help/instruction button with all the interactions/functionalities

--- script.js
+++ script.js
@@ -31,4 +31,24 @@ EBapp.controller("IHCtrler",IHCtrler);
 //-------------------------------------------------------------------------------------
 //log([1,23,4,"d",5,6,7].get(2));
 
+function showInstr(){
+  var msg = "\t\tBoard:"+
+            "\ndblclick board:\tCreate Note"+
+            "\nclick note:\tSelect/Deselect Note"+
+            "\nclick search result:\tLocate Note"+
+            "\npress    x:\t\tDelete Selected Notes"+
+            "\npress    c:\t\tConnect Selected Notes"+
+            "\npress    v:\tView connections of Selected Notes"+
+            "\ndblclick line:\tView connected notes"+
+            "\ndblclick note:\tEdit Note in Editor"+
+            "\n\n\t\tEditor:"+
+            "\nclick connection:\tSave and goto clicked note"+
+            "\npress    esc:\tExit without saving"+
+            "\n\n\t\tOther:"+
+            "\nNotes:\t draggable"+
+            "\nBoard:\t draggable"
+
+  alert(msg);
+}
+showInstr();
 //log("done loading script.js");
